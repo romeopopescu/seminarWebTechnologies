652a934844a3611cca56e41ac2f82110518f0b1f
Romeo <popescuromeo22@stud.ase.ro>
2024-10-16 17:08:58 +0300

    Finished S01D03

--- week1/scripts/script.js
+++ week1/scripts/script.js
@@ -2,6 +2,34 @@
 let sayHello = (name) => {
     return `Hello, ${name}!`;
 }
-
 console.log(sayHello('Matei'));
 
+//S01D02
+function isDivisible(nr, divizor) {
+    if (nr % divizor == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(isDivisible(10, 3));
+console.log(isDivisible(10, 2));
+
+//S01D03
+//count how many instances of a letter in a word 
+function countLetters(word, letter) {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        if (letter === word.charAt(i)) {
+            count++;
+        }
+    }
+    return count;
+} 
+let occurences = (word, letter) => word.split(letter).length - 1; 
+ 
+console.log(countLetters('andreininin', 'n'));
+console.log(occurences('andreininin', 'n'));
+
+//S01D04
